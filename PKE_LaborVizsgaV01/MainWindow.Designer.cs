b8efac7ec7d75660ca0bf1150b03c7d3da45a87c
95HNR <megnemmondodki@gmail.com>
2025-11-06 12:43:51 +0200

    Step1
    Form 1 atnevezve MainWindowra, felulet elkeszitve
    Szukseges packagek telepitve.

---
+++ PKE_LaborVizsgaV01/MainWindow.Designer.cs
@@ -0,0 +1,351 @@
+﻿namespace UMFST.MIP.Variant1_Bookstore
+{
+    // Class name changed
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControlMain = new System.Windows.Forms.TabControl();
+            this.tabPageBooks = new System.Windows.Forms.TabPage();
+            this.dgvBooks = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnRestock = new System.Windows.Forms.Button();
+            this.btnFilter = new System.Windows.Forms.Button();
+            this.txtSearchBooks = new System.Windows.Forms.TextBox();
+            this.tabPageOrders = new System.Windows.Forms.TabPage();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.dgvOrders = new System.Windows.Forms.DataGridView();
+            this.dgvOrderItems = new System.Windows.Forms.DataGridView();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lblOrderTotal = new System.Windows.Forms.Label();
+            this.tabPageReports = new System.Windows.Forms.TabPage();
+            this.txtReport = new System.Windows.Forms.TextBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lblStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tabControlMain.SuspendLayout();
+            this.tabPageBooks.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBooks)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.tabPageOrders.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOrders)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOrderItems)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.tabPageReports.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControlMain
+            // 
+            this.tabControlMain.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControlMain.Controls.Add(this.tabPageBooks);
+            this.tabControlMain.Controls.Add(this.tabPageOrders);
+            this.tabControlMain.Controls.Add(this.tabPageReports);
+            this.tabControlMain.Location = new System.Drawing.Point(12, 41);
+            this.tabControlMain.Name = "tabControlMain";
+            this.tabControlMain.SelectedIndex = 0;
+            this.tabControlMain.Size = new System.Drawing.Size(776, 384);
+            this.tabControlMain.TabIndex = 0;
+            // 
+            // tabPageBooks
+            // 
+            this.tabPageBooks.Controls.Add(this.dgvBooks);
+            this.tabPageBooks.Controls.Add(this.panel1);
+            this.tabPageBooks.Location = new System.Drawing.Point(4, 22);
+            this.tabPageBooks.Name = "tabPageBooks";
+            this.tabPageBooks.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageBooks.Size = new System.Drawing.Size(768, 358);
+            this.tabPageBooks.TabIndex = 0;
+            this.tabPageBooks.Text = "Books";
+            this.tabPageBooks.UseVisualStyleBackColor = true;
+            // 
+            // dgvBooks
+            // 
+            this.dgvBooks.AllowUserToAddRows = false;
+            this.dgvBooks.AllowUserToDeleteRows = false;
+            this.dgvBooks.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvBooks.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvBooks.Location = new System.Drawing.Point(3, 43);
+            this.dgvBooks.MultiSelect = false;
+            this.dgvBooks.Name = "dgvBooks";
+            this.dgvBooks.ReadOnly = true;
+            this.dgvBooks.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvBooks.Size = new System.Drawing.Size(762, 312);
+            this.dgvBooks.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnRestock);
+            this.panel1.Controls.Add(this.btnFilter);
+            this.panel1.Controls.Add(this.txtSearchBooks);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(762, 40);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnRestock
+            // 
+            this.btnRestock.Location = new System.Drawing.Point(344, 8);
+            this.btnRestock.Name = "btnRestock";
+            this.btnRestock.Size = new System.Drawing.Size(94, 23);
+            this.btnRestock.TabIndex = 2;
+            this.btnRestock.Text = "Restock +10";
+            this.btnRestock.UseVisualStyleBackColor = true;
+            this.btnRestock.Click += new System.EventHandler(this.btnRestock_Click);
+            // 
+            // btnFilter
+            // 
+            this.btnFilter.Location = new System.Drawing.Point(263, 8);
+            this.btnFilter.Name = "btnFilter";
+            this.btnFilter.Size = new System.Drawing.Size(75, 23);
+            this.btnFilter.TabIndex = 1;
+            this.btnFilter.Text = "Search";
+            this.btnFilter.UseVisualStyleBackColor = true;
+            this.btnFilter.Click += new System.EventHandler(this.btnFilter_Click);
+            // 
+            // txtSearchBooks
+            // 
+            this.txtSearchBooks.Location = new System.Drawing.Point(4, 10);
+            this.txtSearchBooks.Name = "txtSearchBooks";
+            this.txtSearchBooks.Size = new System.Drawing.Size(253, 20);
+            this.txtSearchBooks.TabIndex = 0;
+            // 
+            // tabPageOrders
+            // 
+            this.tabPageOrders.Controls.Add(this.splitContainer1);
+            this.tabPageOrders.Location = new System.Drawing.Point(4, 22);
+            this.tabPageOrders.Name = "tabPageOrders";
+            this.tabPageOrders.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageOrders.Size = new System.Drawing.Size(768, 358);
+            this.tabPageOrders.TabIndex = 1;
+            this.tabPageOrders.Text = "Orders";
+            this.tabPageOrders.UseVisualStyleBackColor = true;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(3, 3);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.dgvOrders);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.dgvOrderItems);
+            this.splitContainer1.Panel2.Controls.Add(this.panel2);
+            this.splitContainer1.Size = new System.Drawing.Size(762, 352);
+            this.splitContainer1.SplitterDistance = 175;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // dgvOrders
+            // 
+            this.dgvOrders.AllowUserToAddRows = false;
+            this.dgvOrders.AllowUserToDeleteRows = false;
+            this.dgvOrders.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvOrders.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvOrders.Location = new System.Drawing.Point(0, 0);
+            this.dgvOrders.MultiSelect = false;
+            this.dgvOrders.Name = "dgvOrders";
+            this.dgvOrders.ReadOnly = true;
+            this.dgvOrders.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvOrders.Size = new System.Drawing.Size(762, 175);
+            this.dgvOrders.TabIndex = 0;
+            this.dgvOrders.SelectionChanged += new System.EventHandler(this.dgvOrders_SelectionChanged);
+            // 
+            // dgvOrderItems
+            // 
+            this.dgvOrderItems.AllowUserToAddRows = false;
+            this.dgvOrderItems.AllowUserToDeleteRows = false;
+            this.dgvOrderItems.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvOrderItems.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvOrderItems.Location = new System.Drawing.Point(0, 30);
+            this.dgvOrderItems.Name = "dgvOrderItems";
+            this.dgvOrderItems.ReadOnly = true;
+            this.dgvOrderItems.Size = new System.Drawing.Size(762, 143);
+            this.dgvOrderItems.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.lblOrderTotal);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(762, 30);
+            this.panel2.TabIndex = 0;
+            // 
+            // lblOrderTotal
+            // 
+            this.lblOrderTotal.AutoSize = true;
+            this.lblOrderTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOrderTotal.Location = new System.Drawing.Point(4, 7);
+            this.lblOrderTotal.Name = "lblOrderTotal";
+            this.lblOrderTotal.Size = new System.Drawing.Size(103, 15);
+            this.lblOrderTotal.TabIndex = 0;
+            this.lblOrderTotal.Text = "Total: 0.00 EUR";
+            // 
+            // tabPageReports
+            // 
+            this.tabPageReports.Controls.Add(this.txtReport);
+            this.tabPageReports.Controls.Add(this.panel3);
+            this.tabPageReports.Location = new System.Drawing.Point(4, 22);
+            this.tabPageReports.Name = "tabPageReports";
+            this.tabPageReports.Size = new System.Drawing.Size(768, 358);
+            this.tabPageReports.TabIndex = 2;
+            this.tabPageReports.Text = "Reports";
+            this.tabPageReports.UseVisualStyleBackColor = true;
+            // 
+            // txtReport
+            // 
+            this.txtReport.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtReport.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtReport.Location = new System.Drawing.Point(0, 40);
+            this.txtReport.Multiline = true;
+            this.txtReport.Name = "txtReport";
+            this.txtReport.ReadOnly = true;
+            this.txtReport.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtReport.Size = new System.Drawing.Size(768, 318);
+            this.txtReport.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.btnExport);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(768, 40);
+            this.panel3.TabIndex = 0;
+            // 
+            // btnExport
+            // 
+            this.btnExport.Location = new System.Drawing.Point(4, 8);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(101, 23);
+            this.btnExport.TabIndex = 0;
+            this.btnExport.Text = "Export Report";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.ForeColor = System.Drawing.Color.DarkRed;
+            this.btnReset.Location = new System.Drawing.Point(12, 12);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(111, 23);
+            this.btnReset.TabIndex = 1;
+            this.btnReset.Text = "Reset Database";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblStatus});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(39, 17);
+            this.lblStatus.Text = "Ready";
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.tabControlMain);
+            this.Name = "MainWindow";
+            this.Text = "BookStore Manager (Variant 1)";
+            this.Load += new System.EventHandler(this.MainWindow_Load);
+            this.tabControlMain.ResumeLayout(false);
+            this.tabPageBooks.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBooks)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.tabPageOrders.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOrders)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOrderItems)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.tabPageReports.ResumeLayout(false);
+            this.tabPageReports.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControlMain;
+        private System.Windows.Forms.TabPage tabPageBooks;
+        private System.Windows.Forms.TabPage tabPageOrders;
+        private System.Windows.Forms.TabPage tabPageReports;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lblStatus;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnFilter;
+        private System.Windows.Forms.TextBox txtSearchBooks;
+        private System.Windows.Forms.DataGridView dgvBooks;
+        private System.Windows.Forms.Button btnRestock;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.DataGridView dgvOrders;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lblOrderTotal;
+        private System.Windows.Forms.DataGridView dgvOrderItems;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.TextBox txtReport;
+    }
+}
